ece3eb57dc9167c24a6eb59b5b5d965776d319d9
mailsondias7 <mailsondias7@gmail.com>
2020-08-04 22:26:09 -0300

    criação do css - mobile first

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,12 +4,13 @@ import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import './styles.css'
 
 function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div className="logo-containter">
+        <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
@@ -21,18 +22,20 @@ function Landing() {
         />
 
         <div className="buttons-container">
-            <a href="" className="study">
-                <img src={studyIcon} alt="Estudar"/>
-                Estudar
-            </a>
-            <a href="" className="give-classes">
-                <img src={giveClassesIcon} alt="Estudar"/>
-                Estudar
-            </a>
+          <a href="https://github.com" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+
+          <a href="https://github.com" className="give-classes">
+            <img src={giveClassesIcon} alt="Estudar" />
+            Estudar
+          </a>
         </div>
 
         <span className="total-connections">
-            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de 200 conexões já realizadas{' '}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
